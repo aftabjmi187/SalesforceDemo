e7c53fa6ec8dfe0bed8b0d74f2dd2306e5347916
Aftab Alam <64343731+aftabjmi187@users.noreply.github.com>
2025-04-16 02:33:00 +0530

    Update server.js
    require-playwright added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { chromium } = require('playwright');
+require('playwright/install').install({ browsers: ['chromium'] }); 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
